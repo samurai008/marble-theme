6d09da29dd20bcdc1ead4c2f2fd5f6a8244430d1
Michael Wales <webmaster@michaelwales.com>
2017-09-08 10:55:45 +0530

    fm

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var menuToggler = document.getElementById('menu-toggler');
+var backDrop = document.getElementById('backdrop');
+
+function setBackDrop(prop) {
+    backDrop.style.cssText = "display: " +  prop;
+}
+
+setBackDrop('none');
+
+menuToggler.addEventListener('click', function() {
+    if (backDrop.style.display == 'none')
+        setBackDrop('block');
+    else
+        setBackDrop('none');
+})
+
+backDrop.addEventListener('click', function() {
+    menuToggler.click();
+    setBackDrop('none');
+})
